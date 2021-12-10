040fca4a5e0a408d8c283c480c78a0dceaf3bfad
Musthafa Faishal <musthafafaishal@gmail.com>
2021-12-10 13:22:47 +0700

    Integration test 2

--- specs/favoriteRestaurantSpec.js
+++ specs/favoriteRestaurantSpec.js
@@ -16,4 +16,20 @@ describe("Favoriting a Restaurant", () => {
       document.querySelector('[aria-label="like this restaurant"]')
     ).toBeTruthy();
   });
+
+  it("should not show the unfavorite button when the restaurant has not been favorited before", async () => {
+    document.body.innerHTML = '<div id="favoriteButtonContainer"></div>';
+    await FavoriteButtonInitiator.init({
+      favoriteButtonContainer: document.querySelector(
+        "#favoriteButtonContainer"
+      ),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    expect(
+      document.querySelector('[aria-label="unlike this restaurant"]')
+    ).toBeFalsy();
+  });
 });
